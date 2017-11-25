d796c535ba6293513cc1a60edf1f298c8b4a3b9d
Samuel Franssens <u0043016@kuleuven.be>
2017-11-25 14:53:18 +0100

    minor updates

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # champions_league_draw
-Last year, Arsenal FC asked me to calculate the odds of drawing each of their possible opponents in the knock-out phase of the Champions League. This is the R script I created for that.
+In 2014, Arsenal FC asked me to calculate the odds of drawing each of their possible opponents in the knock-out phase of the Champions League. This is the R script I created for that.
--- cl_draw.r
+++ cl_draw.r
@@ -13,39 +13,54 @@
 # We can then see how many times each team is drawn against each opponent.
 
 
+# teams that made it through ----------------------------------------------
+# add the teams in the right order (group A winner, group A runner up, group B winner, group B runner up, ...)
+# add the teams' countries as well with a country code of your choice
 
-# away teams = winners, put them in the correct order (group A, group B, group C, ...)
-# home teams = runners up, in correct order
-awayteams <- c("atletico", "real",  "monaco",    "dortmund","munchen","barcelona", "chelsea", "porto")  
-hometeams <- c("juventus", "basel", "leverkusen","arsenal", "city",   "psg",       "schalke", "donetsk")
+A1 <- c("united","e")
+A2 <- c("basel","sw")
+B1 <- c("psg","f")
+B2 <- c("bayern","d")
+C1 <- c("chelsea","e")
+C2 <- c("roma","i")
+D1 <- c("barcelona","s")
+D2 <- c("juventus","i")
+E1 <- c("liverpool","e")
+E2 <- c("sevilla","s")
+F1 <- c("city","e")
+F2 <- c("donetsk","u")
+G1 <- c("besiktas","t")
+G2 <- c("porto","p")
+H1 <- c("tottenham","e")
+H2 <- c("madrid","s")
 
-nrgames <- length(awayteams) # how many games will be played in the next round?
+teams <- as.data.frame(cbind(rbind(A1,B1,C1,D1,E1,F1,G1,H1),rbind(A2,B2,C2,D2,E2,F2,G2,H2)))
+names(teams) <- c("away","away.country","home","home.country") # winners will be away team in next game
+nrgames <- nrow(teams) # how many games will be played in the next round?
 
-# create a matrix for games that are not allowed
+
+# constraints (which teams cannot be drawn against each other) ------------
 # constraints1 = teams from same group
 constraints1 <- matrix(data=NA,nrow=nrgames,ncol=2) 
-constraints1[,1] <- hometeams
-constraints1[,2] <- awayteams
+constraints1[,1] <- levels(teams$home)[teams$home]
+constraints1[,2] <- levels(teams$away)[teams$away]
 
 # constraints2 = teams from same country
-constraints2 <- matrix(data=NA,nrow=7,ncol=2)   # adjust ncol to the amount of constraints you put in below
-constraints2[1,] <- c("leverkusen","dortmund")
-constraints2[2,] <- c("leverkusen","munchen")
-constraints2[3,] <- c("arsenal","chelsea")
-constraints2[4,] <- c("city","chelsea")
-constraints2[5,] <- c("psg","monaco")
-constraints2[6,] <- c("schalke","dortmund")
-constraints2[7,] <- c("schalke","munchen")
+constraints2 <- matrix(data=NA,nrow=0,ncol=2)
+countries <- unique(c(levels(teams$away.country),levels(teams$home.country)))
+for (i in 1:length(countries)){
+  dummy <- expand.grid(teams$home[teams$home.country == countries[i]], teams$away[teams$away.country == countries[i]])
+  constraints2 <- rbind(constraints2,dummy)
+}
 
-# for every new season, just change the home and away teams above and put in the constraints, and that should do the trick
+# put the constraints together in 1 dataframe
+constraints <- rbind(constraints1,as.matrix(constraints2)) 
 
-constraints <- rbind(constraints1,constraints2) # put the constraints together in 1 dataframe
-amountofconstraints <- nrow(constraints)        # create a variable with the amount of constraints
-
-# constraints are set, begin the simulation -------------------------------
+# create matrix for draws -------------------------------
 k = 1
 totalamountofsimulations <- 1 # Keep track of how many simulations you need to draw to get a certain amount of "good" ones. By good I mean a simulation that does not violate any constraint.
-simulations <- 10000          # Set the amount of desired "good" simulations.
+simulations <- 10000 # Set the amount of desired "good" simulations.
+amountofconstraints <- nrow(constraints) # create a variable with the amount of constraints
 
 draw <- matrix (data=NA,nrow=simulations,ncol=nrgames*2+amountofconstraints+nrgames) 
 # Create a dataset. Each row = one "good" simulation. 
@@ -53,17 +68,16 @@ draw <- matrix (data=NA,nrow=simulations,ncol=nrgames*2+amountofconstraints+nrga
 # column 9 = away team for game 1, column 10 = away team for game 2, ...
 # column 17 checks whether constraint 1 is violated in any of the games, column 18 checks ... constraint 2 ..., ...
 # the last 8 columns give the opponent for hometeam1 = juventus, opponent for hometeam2 = basel, ...
-colnames(draw) <- c( paste( rep("game",16), c(seq(1:nrgames),seq(1:nrgames)), c(rep("_home_team",8),rep("_away_team",8) ),sep="") , paste("constraint",seq(1:amountofconstraints),sep="") , paste(hometeams,"_opponent",sep=""))
+colnames(draw) <- c( paste( rep("game",nrgames*2), c(seq(1:nrgames),seq(1:nrgames)), c(rep("_home_team",nrgames),rep("_away_team",nrgames) ),sep="") , paste("constraint",seq(1:amountofconstraints),sep="") , paste(teams$home,"_opponent",sep=""))
 
-firstconstraintcolumn <- nrgames*2+1                        # in this particular case this is: 8*2+1  = 17
-lastconstraintcolumn  <- nrgames*2 + amountofconstraints    # in this particular case this is: 8*2+15 = 31
+firstconstraintcolumn <- nrgames*2+1                        # in case of CL: 8*2+1  = 17
+lastconstraintcolumn  <- nrgames*2 + amountofconstraints    # in case of CL: 8*2+15 = 31
 
-
-# dataset for draw is created, begin the draw -----------------------------
+# begin the draw -----------------------------
 while (k < simulations+1) { # start simulation
   # One simulation = One unconstrained draw (i.e., could include games that are disallowed)
   # Draw 8 home teams, draw 8 away teams, and then put in zero's where the constraints are (these zeroes will be replaced soon)
-  draw[k,] <- c(sample(hometeams,nrgames,replace=F),sample(awayteams,nrgames,replace=F),numeric(amountofconstraints+nrgames))
+  draw[k,] <- c(sample(levels(teams$home),nrgames,replace=F),sample(levels(teams$away),nrgames,replace=F),numeric(amountofconstraints+nrgames))
   violation_already <- 0 # Later we will go through each of the constraints. As soon as one is violated, there is no need to check the other constraints.
   
   for (j in 1:amountofconstraints) 
@@ -79,19 +93,19 @@ while (k < simulations+1) { # start simulation
     }
   }
   
-  
   violations <- sum(as.numeric(draw[k,firstconstraintcolumn:lastconstraintcolumn])) # any violations?
   if(violations==0){k <- k+1} # only if no violations, go on with next simulation ; if violation, redo k
   totalamountofsimulations <- totalamountofsimulations + 1 # keep track of how many good+bad simulations are necessary to get the desired amount of good simulations
-  
 }
 
+
+# Draw is done. Who plays who? ------------------------------------------------------------
 # By now we have a dataset with 8 games in it that do not violate any constraints.
 # Now we concentrate on the last 8 columns: For each draw, who is the opponent of hometeam1, who is the opponent of hometeam2, ...
 
 for (k in 1:simulations) {
   for (l in 1:nrgames)  {                                # go through each home team and check who is the opponent
-    opponent <- which(draw[k,]==hometeams[l]) + nrgames  # in which column can I find hometeam l's opponent
+    opponent <- which(draw[k,]==teams$home[l]) + nrgames # in which column can I find hometeam l's opponent
     correctcolumn <- nrgames*2+amountofconstraints+l     # in which column should I put hometeam l's opponent
     draw[k,correctcolumn] <- draw[k,opponent]            # put hometeam l's opponent in the correct column
   }
@@ -104,10 +118,12 @@ for (i in 1:nrgames)   # for each home team
 {
   for (k in 1:nrgames) # for each away team
   {
-    probabilities[k,i] <- length(which(draw[,nrgames*2+amountofconstraints+i]==awayteams[k]))/simulations*100
+    probabilities[k,i] <- length(which(draw[,nrgames*2+amountofconstraints+i]==teams$away[k]))/simulations*100
   }
 }
 
-rownames(probabilities) <- awayteams
-colnames(probabilities) <- hometeams
+# probabilities are calculated --------------------------------------------
+rownames(probabilities) <- teams$away
+colnames(probabilities) <- teams$home
 probabilities
+# write.csv(probabilities,"prob.csv")
